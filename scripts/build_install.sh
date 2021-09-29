6fa0e1e91446be1538b9a116f5a6749bd9e3cfb5
inql <dbinkus96@gmail.com>
2021-09-29 23:01:38 +0200

    Remove building exe by default

--- scripts/build_install.sh
+++ scripts/build_install.sh
@@ -6,7 +6,7 @@ then
 fi
 mkdir build && cd build
 directory=`pwd`
-cmake .. -DCMAKE_INSTALL_PREFIX=${directory}
+cmake .. -DOnePass_BUILD_EXECUTABLE=1 -DCMAKE_INSTALL_PREFIX=${directory}
 cmake --build . --target install
 ./bin/OnePass
 ctest -C Release
