6e24d6bd6bcb70fdac4de5e4a155fcea68ea85ef
inql <dbinkus96@gmail.com>
2021-10-20 01:18:59 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include "onepass/clientWrapper.hpp"
+#include "onepass/client.hpp"
+using namespace onepass::core;
+using namespace onepass::storage;
+using namespace onepass::client;
 
 int main(int argc, char *argv[]){
+    ClientWrapper cw(argc, argv);
+    return Client::handle(cw);
 }
